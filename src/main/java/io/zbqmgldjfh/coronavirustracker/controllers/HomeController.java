5f0cbb7f1f9f62f80ee666fd75c8479375984f2d
jiwoo <zbqmgldjfh@gmail.com>
2021-11-13 16:46:49 +0900

    화면개발, 컨트롤러 완성

---
+++ src/main/java/io/zbqmgldjfh/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,32 @@
+package io.zbqmgldjfh.coronavirustracker.controllers;
+
+import io.zbqmgldjfh.coronavirustracker.models.LocationStats;
+import io.zbqmgldjfh.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    private final CoronaVirusDataService coronaVirusDataService;
+
+    @Autowired
+    public HomeController(CoronaVirusDataService service) {
+        this.coronaVirusDataService = service;
+    }
+
+    @GetMapping("/")
+    public String home(Model model){
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalCases", totalCases);
+        return "home";
+    }
+}
